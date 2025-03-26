6309f394a89960909f1319c538cfc90eeb07f5e0
Jessa Azarcon <yishangazarcom@gmail.com>
2025-03-26 14:13:00 +0800

    Update login.php

--- login.php
+++ login.php
@@ -36,7 +36,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["login"])) {
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>CICTEsports Login</title>
+  <title>CICT Esports Login</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
